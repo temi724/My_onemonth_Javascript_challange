6506fec6d6f58b44396478c4ade1e4f5df5ca000
temi724 <tayodaniel0@gmail.com>
2023-03-13 23:19:37 +0100

    hello world

---
+++ Helloworld/hello.js
@@ -0,0 +1,20 @@
+/**
+ * Welcome to our first javascript tutorial..
+ * In this section we will learn the basics of javascript
+ */
+
+// variable declaration....
+var a = 10
+var b = 20
+
+//using the const key word..
+const c = 25
+
+//Other types in js..
+// Declares a block-scoped, local variable, optionally initializing it to a value.
+let d = 20
+var e = "20"
+//Type casting..
+
+
+console.log(a + b + c + d + parseInt(e))
